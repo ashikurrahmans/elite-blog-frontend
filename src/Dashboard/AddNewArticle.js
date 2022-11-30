59149728928917e92d9912099e43b50c12496ee4
ashikurrahmans <ashiqur.dream@gmail.com>
2022-11-30 21:46:12 +0600

    Changes on add

--- src/Dashboard/AddNewArticle.js
+++ src/Dashboard/AddNewArticle.js
@@ -5,238 +5,240 @@ import JoditEditor from "jodit-react";
 import { allContext } from "../ContextApi/ContentProvider";
 import { useForm } from "react-hook-form";
 
-
-
 const AddNewArticle = () => {
-
+  const { allCategories } = useContext(allContext);
   const [startDate, setStartDate] = useState(new Date());
-  const { blogs,allCategories } = useContext(allContext);
+  const [featureImage, setfeatureImage] = useState(null); // Image Current url after upload
 
-const [postSlug,setPostSlug]= useState('') // Slug
-
-
-  const [config,setConfig] = useState({
-    readonly: false,
-    minHeight: 500
-  })
-
-  // form handeling 
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = data => fullData(data)
-const [mainConent,setMainContent] = useState("")    
-
-const publishTime = startDate.toLocaleDateString()
-const fullData = (data) =>{
-  
-  const authorImg = (data.authorImage[0])
-  const featuredImg = (data.featureImage[0])
-
-  
-  const addPost = {...data,mainConent,publishTime}
-  console.log((addPost))
- }
+  const [postSlug, setPostSlug] = useState(""); // Slug
 
 
-
-
-
-
-  // slug
-  // const [categorySlug,setCategorySlug]= useState('')
-  // const findCategory = (data, property) => {
-  //   let newValue = data?.map((currentElm) => {
-  //     return currentElm[property];
-  //   });
-  //   newValue = [...new Set(newValue)];
-  //   return newValue;
-  // };
-
-  // const categories = findCategory(blogs, "category");
-
-  // Capitalization the categories 
-
-  function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-
-// category handler for making it like title slug
-
-    const makingPostSlug = postSlug.toLowerCase()
+  const titleSlug = postSlug
+    .toLowerCase()
     .replace(/ /g, "-")
     .replace(/[^\w-]+/g, "");
 
 
 
+  const uploadImage = async (e) => {
+    var file = e.target.files[0];
+    var formdata = new FormData();
+
+    formdata.append("file", file);
+    formdata.append("cloud_name", "dbpiftrij");
+    formdata.append("upload_preset", "mpght7ap");
+
+    let res = await fetch(
+      "https://api.cloudinary.com/v1_1/dbpiftrij/auto/upload",
+      {
+        method: "post",
+        mode: "cors",
+        body: formdata,
+      }
+    );
+    let json = await res.json();
+    setfeatureImage(json.secure_url);
+  };
+
+  const [config ] = useState({
+    readonly: false,
+    minHeight: 500,
+  });
+
+  // form handeling
+  const {
+    register,
+    handleSubmit,
+  } = useForm();
+  const onSubmit = (data) => fullData(data);
+  const [mainConent, setMainContent] = useState("");
+
+  const publishTime = startDate.toLocaleDateString();
+
+  const fullData = (data) => {
+    console.log(featureImage);
+    const addPost = { ...data, mainConent, publishTime, featureImage ,titleSlug};
+    console.log(addPost);
+  };
+
+  // Capitalization the categories
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+
+
+
+
+
+  // ===============================================================================
+
   return (
-
-      <div className="py-12">
-        <div className="max-w-full mx-auto sm:px-6 lg:px-8">
-          <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div className="p-6 bg-white border-b border-gray-200">
-              <form  onSubmit={handleSubmit(onSubmit)}>
-                <div className="grid lg:grid-cols-2 sm:grid-cols-1 gap-6">
-                  <div className="mb-4">
-                    <label className="text-xl text-gray-600">
-                      Post Title <span className="text-red-500">*</span>
-                    </label>
-                    <br />
-                    <input
-                      type="text"
-                      className="border-2 border-gray-300 p-2 w-full"
-                      name="title"
-                      id="title"
-                      placeholder="Write your title here"
-                      {...register("title")}
-                      onChange={(e)=>setPostSlug(e.target.value)}
-
-                    />
-                  </div>
-                  <div className="mb-4">
-                    <label className="text-xl text-gray-600">
-                      Post Slug <span className="text-red-500">*</span>
-                    </label>
-                    <br />
-                    <input
-                      type="text"
-                      className="border-2 border-gray-300 p-2 w-full"
-                      name="titleSlug"
-                      id="titleSlug"
-                      placeholder="sample-demo-title"
-                      {...register("titleSlug")}
-                      value={postSlug ? makingPostSlug : "" }
-                />
-                  </div>
-                  <div className="mb-4">
-                    <label className="text-xl text-gray-600">
-                      Author Name <span className="text-red-500">*</span>
-                    </label>
-                    <br />
-                    <input
-                      type="text"
-                      className="border-2 border-gray-300 p-2 w-full"
-                      name="authorName"
-                      id="authorName"
-                      placeholder="Write the name of author"
-                      {...register("authorName")}
-                    />
-                  </div>
-                  {/* // Author Image Choose  */}
-                  <div className="mb-4">
-                    <label className="text-xl text-gray-600">
-                      Author Image <span className="text-sm text-red-500 ml-2">(optional)</span>
-                    </label>
-                    <br />
-                    <input
-                      type="file"
-                      className="border-2 border-gray-300 p-2 w-full"
-                      name="authorImage"
-                      id="authorImage"
-                      placeholder="Choose Your Author Image"
-                      {...register("authorImage")}
-                    />
-                  </div>
-                  <div className="mb-4">
-                    <label className="text-xl text-gray-600">
-                      Post Categories <span className="text-red-500">*</span>
-                    </label>
-
-                    <select
-                      id="category"
-                      name="category"
-                      {...register("category")}
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                       >
-                      <option  defaultValue="Choose a category">Choose a category</option>
-                      {
-                        allCategories?.map((category,index)=> <option key={index} value={category?.categoryName}>{capitalizeFirstLetter(category?.categoryName)}</option> )
-                      }
-                      
-                    </select>
-                  </div>
-                  <div className="mb-4">
-                    <label className="text-xl text-gray-600">
-                      Choose publish date
-                      <span className="text-red-500">*</span>
-                    </label>
-                    <br />
-
-                    <DatePicker
-                      selected={startDate}
-                      minDate={new Date()}
-                      dateFormat="dd/MM/yyyy"
-                      onChange={(date) => setStartDate(date)}
-                      isClearable
-                      placeholderText="I have been cleared!"
-                      className="border-2 border-gray-300 p-2 w-full"
-                      name="publishTime"
-                      {...register("publishTime")}
-        
-                    />
-                  </div>
-
-
-      
-                  <div className="flex justify-center items-center w-full my-6 ">
-                    <label
-                      htmlFor="dropzone-file"
-                      className="flex flex-col items-center justify-center w-full h-22 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-                    >
-                      <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                        <svg
-                          className="w-10 h-10 mb-3 text-gray-400"
-                          fill="none"
-                          stroke="currentColor"
-                          viewBox="0 0 24 24"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-                          ></path>
-                        </svg>
-                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                          <span className="font-semibold">Click to upload</span>
-                          or drag and drop
-                        </p>
-                        <p className="text-xs text-gray-500 dark:text-gray-400">
-                          SVG, PNG, JPG or GIF (MAX. 800x400px)
-                        </p>
-                      </div>
-                      <input
-                        id="dropzone-file"
-                        type="file"
-                        className="hidden"
-                        name="featureImage"
-                        {...register("featureImage")}
-                        />
-                    </label>
-                  </div>
+    <div className="py-12">
+      <div className="max-w-full mx-auto sm:px-6 lg:px-8">
+        <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+          <div className="p-6 bg-white border-b border-gray-200">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="grid lg:grid-cols-2 sm:grid-cols-1 gap-6">
+                <div className="mb-4">
+                  <label className="text-xl text-gray-600">
+                    Post Title <span className="text-red-500">*</span>
+                  </label>
+                  <br />
+                  <input
+                    type="text"
+                    className="border-2 border-gray-300 p-2 w-full"
+                    name="title"
+                    id="title"
+                    placeholder="Write your title here"
+                    onChange={(e) => setPostSlug(e.target.value)}
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="text-xl text-gray-600">
+                    Post Slug <span className="text-red-500">*</span>
+                  </label>
+                  <br />
+                  <input
+                    type="text"
+                    className="border-2 border-gray-300 p-2 w-full"
+                    name="titleSlug"
+                    id="titleSlug"
+                    placeholder="sample-demo-title"
+                    {...register("titleSlug")}
+                    value={postSlug ? titleSlug : ""}
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="text-xl text-gray-600">
+                    Author Name <span className="text-red-500">*</span>
+                  </label>
+                  <br />
+                  <input
+                    type="text"
+                    className="border-2 border-gray-300 p-2 w-full"
+                    name="authorName"
+                    id="authorName"
+                    placeholder="Write the name of author"
+                    {...register("authorName")}
+                  />
                 </div>
 
-                <div className="mb-8">
-    
-                  <JoditEditor tabIndex={1}  config={config}  onChange={data=> setMainContent(data)}/>
-                </div>
+                <div className="mb-4">
+                  <label className="text-xl text-gray-600">
+                    Post Categories <span className="text-red-500">*</span>
+                  </label>
 
-                <div className="flex p-1">
-            
-                  <button
-                     type="submit"
-                    className="p-3 bg-blue-500 text-white hover:bg-blue-400"
-                    required
+                  <select
+                    id="category"
+                    name="category"
+                    {...register("category")}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   >
-                    Published Article
-                  </button>
+                    <option defaultValue="Choose a category">
+                      Choose a category
+                    </option>
+                    {allCategories?.map((category, index) => (
+                      <option key={index} value={category?.categoryName}>
+                        {capitalizeFirstLetter(category?.categoryName)}
+                      </option>
+                    ))}
+                  </select>
                 </div>
-              </form>
-            </div>
+                <div className="mb-4">
+                  <label className="text-xl text-gray-600">
+                    Choose publish date
+                    <span className="text-red-500">*</span>
+                  </label>
+                  <br />
+
+                  <DatePicker
+                    selected={startDate}
+                    minDate={new Date()}
+                    dateFormat="dd/MM/yyyy"
+                    onChange={(date) => setStartDate(date)}
+                    isClearable
+                    placeholderText="I have been cleared!"
+                    className="border-2 border-gray-300 p-2 w-full"
+                    name="publishTime"
+                  />
+                </div>
+              </div>
+
+              <div className="mb-8">
+                <JoditEditor
+                  tabIndex={1}
+                  config={config}
+                  onChange={(data) => setMainContent(data)}
+                />
+              </div>
+              <div className="grid lg:grid-cols-2 sm:grid-cols-1">
+                <div className="flex justify-center items-center w-full my-6">
+                  <label
+                    htmlFor="dropzone-file"
+                    className="flex flex-col items-center justify-center w-full h-22 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer"
+                  >
+                    <div className="mb-8 relative">
+                      <div>
+                        <input
+                          type="file"
+                          name="featureImage"
+                          id="featureImage"
+                          className="sr-only"
+                          onChange={uploadImage}
+                          
+                        />
+                      </div>
+              
+                      <label
+                        htmlFor="file"
+                        className=" flex min-h-[300px] items-center justify-center  p-12 text-center"
+                      >
+                        <div>
+                          <span className="mb-2 block text-xl font-semibold text-[#07074D]">
+                            Drop files here
+                          </span>
+                          <span className="mb-2 block text-base font-medium text-[#6B7280]">
+                            Or
+                          </span>
+                          <button className="inline-flex rounded border border-[#e0e0e0] py-2 px-7 text-base font-medium text-[#07074D]">
+                            Browse
+                          </button>
+                        </div>
+                      </label>
+                    </div>
+                    <input
+                      id="dropzone-file"
+                      type="file"
+                      className="hidden"
+                      name="featureImage"
+                    />
+                  </label>
+                </div>
+                <div>
+                     <img
+                        src={featureImage && featureImage}
+                        alt="feature"
+                        className="w-[700px] h-[500px] relative "
+                      ></img>
+                </div>
+              </div>
+
+              <div className="flex p-1 my-16">
+                <button
+                  type="submit"
+                  className="p-3 bg-blue-500 text-white hover:bg-blue-400"
+                  required
+                >
+                  Published Article
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
-
+    </div>
   );
 };
 
