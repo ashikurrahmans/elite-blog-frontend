49f5251a46356db46b2878febc52d6ae27119b02
elitedentals <elitedentalseo.usa@gmail.com>
2023-05-03 15:06:26 +0600

    design changes

--- src/Components/Home/HomeTitles.js
+++ src/Components/Home/HomeTitles.js
@@ -7,7 +7,7 @@ const HomeTitles = (props) => {
         <h1 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">
           {props.title}
         </h1>
-        <p className="lg:w-2/3 mx-auto leading-relaxed text-sm">
+        <p className="lg:w-2/3 mx-auto leading-relaxed text-xl">
           {props.description}
         </p>
       </div>
